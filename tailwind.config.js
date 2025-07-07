3a7615f58282e6e3c5d84944c927c2a595bc14c3
Daniel Riêgo <danielriegoor@gmail.com>
2025-07-06 23:45:29 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  // O Tailwind irá procurar por classes nestes arquivos para otimizar o CSS final.
+  content: [
+    "./index.html",
+    "./scripts/main.js"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      // Aqui definimos sua cor personalizada para o tema claro.
+      colors: {
+        'primary-light': 'rgb(203, 221, 236)',
+      },
+    },
   },
   plugins: [],
 }
-
